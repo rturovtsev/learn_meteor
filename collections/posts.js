2a76017aa1a9b9bba8ecd98194d1256a4ab1362f
SuGROB <sugrob.boid@gmail.com>
2015-03-01 19:47:41 +0300

    add new and best

--- collections/posts.js
+++ collections/posts.js
@@ -51,16 +51,10 @@ Meteor.methods({
           throw new Meteor.Error(401, "Надо залогиниться чтобы голосовать");
       }
 
-      var post = Posts.findOne(postId);
-      if (!post) {
-          throw new Meteor.Error(422, "Пост не найден");
-      }
-
-      if (_.include(post.upvoters, user._id)) {
-          throw new Meteor.Error(422, "Вы уже голосовали за этот пост");
-      }
-
-      Posts.update(post._id, {
+      Posts.update({
+          _id: postId,
+          upvoters: {$ne: user._id}
+        }, {
           $addToSet: {upvoters: user._id},
           $inc: {votes: 1}
       });
